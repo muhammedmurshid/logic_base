d9b3be01de674039c2e3eb01dc947c531c6ec680
Murshid <muhsink00000@gmail.com>
2023-11-08 10:40:43 +0530

    added corse level,group,papers

--- models/logic_course.py
+++ models/logic_course.py
@@ -14,6 +14,7 @@ class LogicBaseCourses(models.Model):
     company_id = fields.Many2one('res.company', string="Branch", default=lambda self: self.env.company.id)
     currency_id = fields.Many2one('res.currency', string='Currency', required=True,
                                   default=lambda self: self.env.user.company_id.currency_id)
+    course_levels = fields.Many2many('course.levels', string='Course Levels')
     papers = fields.Many2many('course.papers', 'paper_id', string='Papers')
     state = fields.Selection([('draft', 'Draft'), ('done', 'Done')], default='draft')
     academic_head = fields.Many2one('res.users', string='Academic Head')
@@ -41,3 +42,23 @@ class CoursePapers(models.Model):
     name = fields.Char(string='Paper', required=True)
     course_id = fields.Many2one('logic.base.courses', string='Course')
     paper_id = fields.Many2one('logic.base.courses', string='Course')
+    group_ids = fields.Many2many('course.groups', string='Groups')
+
+
+class CourseLevels(models.Model):
+    _name = 'course.levels'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+    _description = 'Course Level'
+
+    name = fields.Char(string='Level', required=True)
+    groups_ids = fields.Many2many('course.groups', string='Groups')
+    course_id = fields.Many2many('logic.base.courses', string='Course')
+
+
+class CourseGroups(models.Model):
+    _name = 'course.groups'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+    _description = 'Course Group'
+
+    name = fields.Char(string='Group', required=True)
+    level_ids = fields.Many2many('course.levels', string='Levels')
